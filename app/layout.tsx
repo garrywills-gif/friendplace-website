fd39fa5febb17c95bae6efae91b300aa2168c9ae
garrywills-gif <garry.wills@ymail.com>
2026-09-04 19:26:08 +1000

    Load Meta Pixel base code directly in document head

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,6 +63,17 @@ const structuredData = {
   ],
 };
 
+const metaPixelBaseCode = `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '4256953884595956');
+fbq('track', 'PageView');`;
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return <html lang="en-AU"><head><link rel="preconnect" href="https://fonts.googleapis.com"/><link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/><link href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@400;500;600;700;800;900&display=swap" rel="stylesheet"/><script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }} /></head><body><MetaPixel/><CompanionProvider><SiteHeader/><main>{children}</main><SiteFooter/><ConciergeOverlay/><LeadingButterfly/></CompanionProvider></body></html>;
+  return <html lang="en-AU"><head><link rel="preconnect" href="https://fonts.googleapis.com"/><link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/><link href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@400;500;600;700;800;900&display=swap" rel="stylesheet"/><script id="friendplace-meta-pixel" dangerouslySetInnerHTML={{ __html: metaPixelBaseCode }} /><script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }} /></head><body><MetaPixel/><CompanionProvider><SiteHeader/><main>{children}</main><SiteFooter/><ConciergeOverlay/><LeadingButterfly/></CompanionProvider></body></html>;
 }
